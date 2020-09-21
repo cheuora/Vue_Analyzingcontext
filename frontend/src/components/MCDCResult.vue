033ef699d1fc235d5e4e0a809652234dc92ba23d
cheuora <cheuora@gmail.com>
2020-09-21 23:51:37 +0900

    finish

--- frontend/src/components/MCDCResult.vue
+++ frontend/src/components/MCDCResult.vue
@@ -3,7 +3,8 @@
         <h3>MCDC Result</h3>
         <center>
         <p v-html="table"></p>
-                <button @click="download">Down</button>
+        <br>
+                <button @click="downLoad">Down</button>
         </center>
     </div>
 </template>
@@ -13,17 +14,30 @@
 </style>
 
 <script>
+  import axios from 'axios';
+
   export default {
     data() {
       return {
-        table: this.$route.params.tabledata
-        url : "/assets/tmp"
+        table: this.$route.params.tabledata,
+        url: "http://localhost:5000/static/temp/" + this.$route.params.xlsxfile
       }
     },
     methods: {
-      download() {
-        
-      }
+      downLoad() {
+        axios({
+          url: this.url,
+          method: 'GET',
+          responseType: 'blob',
+        }).then((response) => {
+          var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          var fileLink = document.createElement('a');
+          fileLink.href = fileURL;
+          fileLink.setAttribute('download', 'result.xlsx');
+          document.body.appendChild(fileLink);
+          fileLink.click();
+        })
+      },
     },
   } 
 
