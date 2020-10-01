a5f828e3a88e55eb7fcbf703224b10a6a059e622
cheuora <cheuora@gmail.com>
2020-10-02 01:15:27 +0900

    add google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import JsMind from './components/JsMind/index'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 Vue.use(JsMind)
+Vue.use(VueAnalytics,{
+  id : 'UA-112917851-2',
+  router,
+})
 
 new Vue({
   router,
