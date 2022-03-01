0a61d1a66607a56046d1b184f2bece5c39c23906
金成俊 <cheuora@gmail.com>
2022-03-01 12:12:16 +0900

    deprecated flask files

--- backend/runscript.sh
+++ backend/runscript.sh
@@ -1,3 +1,4 @@
 # nginx -s stop and kill uwsgi process beforen run this script
+# This file is deprecated (2022,03,01)
 nginx
 uwsgi start.ini &
--- backend/start.py
+++ backend/start.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# This file is deprecated!!!(2022,03,01)
 
 import logging
 
